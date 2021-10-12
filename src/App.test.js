4567af1b350ef2bc5f448b439594b6e24cce2f57
P4NCH02798 <43188535+P4NCH02798@users.noreply.github.com>
2021-10-12 19:33:38 -0400

    Tercer Test,auto avanza correctamente

--- src/App.test.js
+++ src/App.test.js
@@ -51,3 +51,38 @@ describe("La direccion debe ser correcta", () => {
         expect(girarDer("E")).toEqual("S");
     });
 });
+
+function avanzar(posicionInicial, orientacion, mapa) {
+    var mov;
+    var res;
+    if (orientacion == "N")
+        mov = [0, 1];
+    else if (orientacion == "O")
+        mov = [-1, 0];
+    else if (orientacion == "S")
+        mov = [0, -1];
+    else if (orientacion == "E")
+        mov = [1, 0];
+    res = [posicionInicial[0] + mov[0], posicionInicial[1] + mov[1]];
+    return res;
+}
+describe("Avanzar debe funcionar correctamente", () => {
+    var posicionInicial = [3, 3];
+    var mapa = [5, 5];
+    var posicionNorte = [3, 4];
+    it("Debe avanzar correctamente al norte", () => {
+        expect(avanzar(posicionInicial, 'N', mapa)).toEqual(posicionNorte);
+    });
+    var posicionSur = [3, 2];
+    it("Debe avanzar correctamente al sur", () => {
+        expect(avanzar(posicionInicial, 'S', mapa)).toEqual(posicionSur);
+    });
+    var posicionEste = [4, 3];
+    it("Debe avanzar correctamente al este", () => {
+        expect(avanzar(posicionInicial, 'E', mapa)).toEqual(posicionEste);
+    });
+    var posicionOeste = [2, 3];
+    it("Debe avanzar correctamente al oeste", () => {
+        expect(avanzar(posicionInicial, 'O', mapa)).toEqual(posicionOeste);
+    });
+});
